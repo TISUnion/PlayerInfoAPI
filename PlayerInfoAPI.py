2d60124bb9910eb3736cc26e8258f39298f998da
Fallen_Breath <yqx791125833@gmail.com>
2020-06-08 23:54:32 +0800

    added doc for API and output json to logger if parse fails

--- PlayerInfoAPI.py
+++ PlayerInfoAPI.py
@@ -3,8 +3,6 @@ import copy
 import re
 import ast
 import json
-import json.decoder
-import time
 from threading import Lock
 
 try:
@@ -12,26 +10,42 @@ try:
 except ImportError:
 	import queue as Queue
 
+
 work_queue = {}
 queue_lock = Lock()
 query_count = 0
 
 
 def convertMinecraftJson(text):
+	r"""
+	Convert Minecraft json string into standard json string and json.loads() it
+	Also if the input has a prefix of "xxx has the following entity data: " it will automatically remove it, more ease!
+	Example available inputs:
+	- Alex has the following entity data: {a: 0b, big: 2.99E7, c: "minecraft:white_wool", d: '{"text":"rua"}'}
+	- {a: 0b, big: 2.99E7, c: "minecraft:white_wool", d: '{"text":"rua"}'}
+	- [0.0d, 10, 1.7E9]
+	- {Air: 300s, Text: "\\o/..\""}
+	- "hello"
+	- 0b
+
+	:param str text: The Minecraft style json string
+	:return: Parsed result
+	"""
+
 	# Alex has the following entity data: {a: 0b, big: 2.99E7, c: "minecraft:white_wool", d: '{"text":"rua"}'}
 	# yeet the prefix
 	text = re.sub(r'^.* has the following entity data: ', '', text)  # yeet prefix
 
 	# {a: 0b, big: 2.99E7, c: "minecraft:white_wool", d: '{"text":"rua"}'}
 	# remove letter after number
-	text = re.sub(r'(?<=\d)[a-zA-Z](?=\D)', '', text)
+	text = re.sub(r'(?<=\d)[a-zA-Z](?=(\D|$))', '', text)
 
 	# {a: 0, big: 2.99E7, c: "minecraft:white_wool", d: '{"text":"rua"}'}
 	# add quotation marks to all
 	text = re.sub(r'([a-zA-Z.]+)(?=:)', '"\g<1>"', text)
 
-	# remove unnecessary quotation created by namespaces
 	# {"a": 0, "big": 2.99E7, "c": ""minecraft":white_wool", "d": '{"text":"rua"}'}
+	# remove unnecessary quotation created by namespaces
 	list_a = re.split(r'""[a-zA-Z.]+":', text)  # split good texts
 	list_b = re.findall(r'""[a-zA-Z.]+":', text)  # split bad texts
 	result = list_a[0]
@@ -48,10 +62,10 @@ def convertMinecraftJson(text):
 
 
 def mcSingleQuotationJsonReader(data):
-	part = data
+	part = data  # type: str
 	count = 1
 	while True:
-		spliter = part.split(r"'{", maxsplit=1)  # Match first opening braces
+		spliter = part.split(r"'{", 1)  # Match first opening braces
 		yield spliter[0]
 		if len(spliter) == 1:
 			return  # No more
@@ -94,11 +108,11 @@ def jsonCheck(j):
 		# checking = checking.replace("\\n", "\\\\n")
 		checking = checking.replace(r'\\', "\\")
 		res = json.loads(checking)
-	except json.decoder.JSONDecodeError:
+	except ValueError:
 		try:
 			# Plan B
 			res = ast.literal_eval(checking)
-		except Exception:
+		except:
 			return False
 
 	data = json.dumps({"data": json.dumps(res)})
@@ -120,6 +134,18 @@ def clean_queue():
 
 
 def getPlayerInfo(server, player, path='', timeout=5):
+	"""
+	Use command "data get entity <player> [<path>]" to query the entity data of a player
+	And then parse the Minecraft style json result into standard json string and decode it into python object
+	If it runs in MCDR and rcon is enabled it will use rcon to query, otherwise it will send to command to the stdin of the
+	server and waiting for result.
+
+	:param server: The server object from MCD or MCDR
+	:param str player: The player you want to query data
+	:param str path: The precise data path you want to query
+	:param int or float timeout: Maximum waiting time before server respond if rcon is disabled
+	:return: The result object, or None if query fails
+	"""
 	if len(path) >= 1 and not path.startswith(' '):
 		path = ' ' + path
 	command = 'data get entity {}{}'.format(player, path)
@@ -130,18 +156,21 @@ def getPlayerInfo(server, player, path='', timeout=5):
 		query_count += 1
 		try:
 			server.execute(command)
-			global work_queue
 			result = get_queue(player).get(timeout=timeout)
 		except Queue.Empty:
-			result = 'null'
+			return None
 		finally:
 			query_count -= 1
-	return convertMinecraftJson(result)
+	try:
+		return convertMinecraftJson(result)
+	except json.JSONDecodeError:
+		server.logger.error('Fail to parse Minecraft json {}'.format(result))
+		return None
 
 
 def onServerInfo(server, info):
 	global work_queue
-	if info.isPlayer == 0 and re.fullmatch(r'\w+ has the following entity data: .*', info.content):
+	if info.isPlayer == 0 and re.match(r'^\w+ has the following entity data: .*$', info.content):
 		player = info.content.split(' ')[0]
 		if query_count > 0:
 			get_queue(player).put(info.content)
@@ -153,3 +182,10 @@ def on_info(server, info):
 	info2 = copy.deepcopy(info)
 	info2.isPlayer = info2.is_player
 	onServerInfo(server, info2)
+
+
+if __name__ == '__main__':
+	for line in convertMinecraftJson.__doc__.splitlines():
+		if re.match(r'^\s*- .*$', line):
+			s = line.split('-')[-1]
+			print('{} -> {}'.format(s, convertMinecraftJson(s)))
